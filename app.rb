757daf3371fd63791f19ff62b99bb56b50134149
smolnreadytobrawl <noelani.villanueva@gmail.com>
2020-12-09 15:34:54 -0800

    created get requests for from sinatra

--- app.rb
+++ app.rb
@@ -1,4 +1,14 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+    get '/name' do 
+       "My name is Crystal"
+            
+    end 
+    get '/hometown' do 
+       "My hometown is Annandale, VA"
+    end 
+    get '/favorite-song' do 
+      "My favorite song is 'Scrawny' by the Wallows"
+    end 
 end
